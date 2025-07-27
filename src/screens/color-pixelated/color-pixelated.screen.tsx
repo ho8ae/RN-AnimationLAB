40f1d1aab702e877c0905ca2298fa46e78fae74c
ho8ae <xogh2242@gmail.com>
2025-07-27 17:58:54 +0900

    feat: skia image 로딩 완료

--- src/screens/color-pixelated/color-pixelated.screen.tsx
+++ src/screens/color-pixelated/color-pixelated.screen.tsx
@@ -1,14 +1,35 @@
 // @refresh reset
 import { FC } from 'react';
-import { Text, View } from 'react-native';
 import { useWindowDimensions } from 'react-native';
+import { Canvas, Image, useImage } from '@shopify/react-native-skia';
 
 export const ColorPixelatedScreen: FC = () => {
-  const { width:stageWidth, height:stageHeight } = useWindowDimensions();
-  
+  const { width: stageWidth, height: stageHeight } = useWindowDimensions();
+
+  const image = useImage(
+    'https://www.iesabroad.org/sites/default/files/2023-05/view-seoul-city-skyline-seoul-tower-sunrise-south-korea.jpg',
+  );
+
+  if (!image) {
+    return null;
+  }
+
   return (
-    <View>
-      <Text>Color Pixelated</Text>
-    </View>
+    <Canvas
+      style = {{
+        width: stageWidth,
+        height: stageHeight,
+        backgroundColor: 'black',
+      }}
+    >
+      <Image
+        image={image}
+        x={0}
+        y={0}
+        width={stageWidth}
+        height={stageHeight}
+        fit="fill"
+      />
+    </Canvas>
   );
 };
